efb6839e4335d7c1d45171695c699c3518cb73d7
Pavel Savelev <9173912682pavel@gmail.com>
2026-02-12 11:48:42 +0300

    fix schedule

--- backend/src/films/films.module.ts
+++ backend/src/films/films.module.ts
@@ -4,13 +4,18 @@ import { FilmsController } from './films.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Film, FilmSchema } from './schemas/film.schema';
 import { FilmsRepository } from './repositories/film.repository'; // ← импорт
+import { ScheduleRepository } from './repositories/schedule.repository';
+import { Schedule, ScheduleSchema } from './schemas/film.schedule.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Film.name, schema: FilmSchema }]),
+    MongooseModule.forFeature([
+      { name: Schedule.name, schema: ScheduleSchema },
+    ]),
   ],
   controllers: [FilmsController],
-  providers: [FilmsService, FilmsRepository],
-  exports: [FilmsService, FilmsRepository],
+  providers: [FilmsService, FilmsRepository, ScheduleRepository],
+  exports: [FilmsService, FilmsRepository, ScheduleRepository],
 })
 export class FilmsModule {}
